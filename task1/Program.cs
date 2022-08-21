f1675710c23e52448c5669aeba7ef8299928f990
Rishat <risatnurullin53@gmail.com>
2022-08-21 13:36:55 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Введите размер массива m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер массива n ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[m, n];
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+arr[i, j] = new Random().Next(0, 100);
+Console.Write(arr[i, j] + " ");
+}
+Console.WriteLine();
+}
+
+for (int i = 0; i < m; i++)
+{
+for (int j = 1; j < n;)
+{
+if (arr[i, j - 1] > arr[i, j])
+{
+int min = arr[i, j];
+arr[i, j] = arr[i, j - 1];
+arr[i, j - 1] = min;
+j = 1;
+}
+else
+{ j++; }
+}
+}
+
+Console.WriteLine("Новый массив: ");
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+Console.Write(arr[i, j] + " ");
+}
+Console.WriteLine();
+}
